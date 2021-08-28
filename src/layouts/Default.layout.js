da0f230a5195e093f3171fc528550703c2fff917
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-08-28 21:20:22 +0530

    Created Carousel

--- src/layouts/Default.layout.js
+++ src/layouts/Default.layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar/navbar.component';
+import HeroCarousel from '../components/HeroCarousel/HeroCarousel.component';
 
 const DefaultLayout = (props) => {
     return (
         <>
             <Navbar />
+            <HeroCarousel />
             { props.children }
         </>
     );
