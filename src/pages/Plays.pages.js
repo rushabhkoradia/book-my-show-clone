9da5e43a7a0b1a5cf5c63984bd78ec1f9ca63d01
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-10-18 21:46:41 +0530

    Version 1

--- src/pages/Plays.pages.js
+++ src/pages/Plays.pages.js
@@ -20,7 +20,7 @@ const PlaysPage = () => {
                         <div className="flex flex-wrap">
                             <div className="w-1/2 my-3 md:w-1/3 lg:w-1/4">
                                 <Poster
-                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOSBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314534-zjzubzamfs-portrait.jpg" 
+                                    src="https://in.bmscdn.com/discovery-catalog/evenzts/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOSBTZXA%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00314534-zjzubzamfs-portrait.jpg" 
                                     title="Tvk Cultural presents Ponniyin Selvan"
                                     subtitle="Watch on BookMyShow" 
                                     description="Tamil"
